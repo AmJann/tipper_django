3ee115503dced7558496cbc3a3bcadafeee91236
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-10-26 14:59:10 -0400

    postgres database added to settings.py

--- tipper_django/settings.py
+++ tipper_django/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'tipper_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tipper',
+        'USER': 'tipperuser',
+        'PASSWORD': 'tipper',
+        'HOST': 'localhost'
     }
 }
 
